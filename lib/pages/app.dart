4027efd827a497889c4149632e121f43a743579c
yangfan <whufanyang@gmail.com>
2019-08-21 23:24:17 +0800

    new home page

---
+++ lib/pages/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      home: new Home(),
+      //routes: ,
+    );
+  }
+
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import '../widgets/drawer.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final GlobalKey<ScaffoldState> scaffoldkey = new GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      key: scaffoldkey,
+      drawer: new MyDrawer(),
+      //body: new Shelf(),
+    );
+  }
+}
